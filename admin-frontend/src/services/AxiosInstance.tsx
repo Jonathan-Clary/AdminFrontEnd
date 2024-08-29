4af2140c63bbb57c48e8094e004f0b78f5e189b2
Justin-swinney <Justinswinney1@icloud.com>
2024-08-29 14:06:52 -0400

    url change

--- admin-frontend/src/services/AxiosInstance.tsx
+++ admin-frontend/src/services/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = (token: string | null) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://localhost:9000/',
+    baseURL: 'http://44.201.168.200:9000/',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': token ? `Bearer ${token}` : '',
