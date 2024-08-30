601019d5cdd70bff9cd19ae03acb962c896d9f42
Jonathan Clary <jonathanbclary@gmail.com>
2024-08-30 07:08:50 -0400

    Change baseURL

--- admin-frontend/src/services/AxiosInstance.tsx
+++ admin-frontend/src/services/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = (token: string | null) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://localhost:9000/',
+    baseURL: 'http://18.206.57.93:9000/',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': token ? `Bearer ${token}` : '',
